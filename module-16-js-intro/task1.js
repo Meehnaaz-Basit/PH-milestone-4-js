f3a69e121d11562e1066b20b9eda41135244b405
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-01-30 11:54:50 +0600

    first commit

---
+++ module-16-js-intro/task1.js
@@ -0,0 +1,22 @@
+/*
+
+Task-1
+You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+Input:
+The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+Output:
+Print the result.
+
+
+*/
+let myMoney = 100;
+
+let orangePrice = 20;
+let applesPrice = 30;
+
+let totalCost = orangePrice + applesPrice;
+let remainingMoney = myMoney - totalCost;
+
+console.log(remainingMoney);
