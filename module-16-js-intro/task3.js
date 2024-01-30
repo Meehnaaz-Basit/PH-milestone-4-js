225eafeff60f0716ae6bb60262b9eff7e65ab9eb
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-01-30 11:58:26 +0600

    added module-16 task 3

---
+++ module-16-js-intro/task3.js
@@ -0,0 +1,17 @@
+/*
+
+Task-3
+You task is to divide the given number by 5 and show the remainder as the output.
+
+Input:
+The first line of the input contains the number.
+
+Output:
+Print the remainder.
+
+*/
+let theNumber = 123;
+
+let remainder = theNumber % 5;
+
+console.log(remainder);
