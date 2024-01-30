4964b2f21895106cafbb29b8a3315a94fa4f3de4
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-01-30 12:00:40 +0600

    added module 17 folder and task 1

---
+++ module-17-conditions/task1.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+
+*/
+
+let price = 400;
+if (price >= 500) {
+  console.log(`Free Drinks`);
+} else {
+  console.log(`Drink price 30tk`);
+}
